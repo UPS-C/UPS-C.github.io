b9519e27df024006d1f80791989ff056f344053f
jja13 <69081493+jja13@users.noreply.github.com>
2023-12-03 18:06:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-document.getElementById("ECS").addEventListener("click", function(){
+document.getElementById("buttonMC").addEventListener("click", function(){
   window.open("https://radmanplays.github.io", "_blank");
+});
+document.getElementById("buttonP").addEventListener("click", function(){
+  window.open("https://picles.sytes.net", "_blank");
 });
