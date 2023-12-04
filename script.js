ae0f1b5294b76c90723d0c6e5ac95ebf4e5fbc56
jja13 <69081493+jja13@users.noreply.github.com>
2023-12-03 20:15:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("buttonMC").addEventListener("click", function(){
   window.open("https://radmanplays.github.io", "_blank");
 });
 document.getElementById("buttonP").addEventListener("click", function(){
-  window.open("https://picles.sytes.net", "_blank");
+  window.open("picles", "_blank");
 });
 document.getElementById("darkModeButton").addEventListener("click", function(){
   document.body.style.backgroundColor = "#333";
