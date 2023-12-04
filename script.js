6e8b1a3e12b17f59da179a7e0435aaef30a1942f
jja13 <69081493+jja13@users.noreply.github.com>
2023-12-03 21:48:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById("buttonMC").addEventListener("click", function(){
-  window.open("picles");
+  location.href = "picles";
 });
 document.getElementById("buttonP").addEventListener("click", function(){
-  window.open("picles");
+  location.href = "picles";
 });
 document.getElementById("darkModeButton").addEventListener("click", function(){
   document.body.style.backgroundColor = "#333";
