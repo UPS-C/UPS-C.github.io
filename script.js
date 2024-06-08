3e8df4b68068b7d52a98fea9c4bd5a15becacd65
jja13 <69081493+jja13@users.noreply.github.com>
2024-06-08 09:23:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 document.getElementById("buttonMC").addEventListener("click", function(){
-  location.href = "picles";
+  location.href = "w2d";
 });
+
 document.getElementById("buttonP").addEventListener("click", function(){
-  location.href = "picles";
+  location.href = "w2d";
 });
+
 document.getElementById("buttonW").addEventListener("click", function(){
   location.href = "w2d";
 });
+
 document.getElementById("darkModeButton").addEventListener("click", function(){
   document.body.style.backgroundColor = "#333";
   document.body.style.color = "white";
 });
+
 document.getElementById("lightModeButton").addEventListener("click", function(){
   document.body.style.backgroundColor = "#ffffff";
   document.body.style.color = "black";
